2e638185780d666d0f893a30a13d273a4c5c4683
e1m3m0 <gamateo@gmail.com>
2021-10-13 22:18:26 -0400

    getnotes and savenotes routes completed

--- Develop/routes/apiRoutes/index.js
+++ Develop/routes/apiRoutes/index.js
@@ -1,2 +1,21 @@
-const fs = require("fs");
-const path = require("path");
+const router = require("express").Router();
+const { createNewNote, validateNote, } = require('../../lib/notes');
+const { notes } = require('../../db/db.json');
+
+router.get("/notes", (req, res) => {
+  let data = notes;
+  res.json(data);
+});
+
+router.post("/notes", (req, res) => {
+  if (!validateNote(req.body)) {
+    res.status(400).send("the note if not complete, please add either a title or text.");
+  } else {
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+  }
+});
+
+
+
+module.exports = router;
